3dec78428ac6ea818b542ead2b61098fc2f3d2fd
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-09-28 21:56:32 +0530

    str and other methods are defined

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+from typing import Union
 """
 *************Food*************
 initial deposit        1000.00
@@ -9,34 +10,41 @@ Total: 923.96
 
 
 class Category:
-    def __init__(self, category) -> None:
+    def __init__(self, category: str) -> None:
         self.category = category
         self.ledger = []
         self._bal_amount = 0.00
 
-    def add_ledger(self, amt, description):
+    def __str__(self) -> str:
+        bill = self.category.center(30, '*') + '\n'
+        for i in self.ledger:
+            bill += f'{i.get("description")[:23]:23}' + f'{i.get("amount"):7.2f}' + '\n'
+        bill += f'Total: {self.get_balance():.2f}'
+        return bill
+
+    def add_ledger(self, amt: Union[float, int], description: str):
         ledger_obj = {
             "amount": amt,
             "description": description
         }
         self.ledger.append(ledger_obj)
 
-    def deposit(self, amt, description=None) -> None:
+    def deposit(self, amt: Union[float, int], description: str = 'No Description') -> None:
         self._bal_amount += amt
-        self.add_ledger(self._bal_amount, description)
+        self.add_ledger(amt, description)
 
-    def withdraw(self, amt, description=None) -> bool:
+    def withdraw(self, amt: Union[float, int], description: str = 'No Description') -> bool:
         res = self.check_funds(amt)
         if res:
             self._bal_amount -= amt
-            self.add_ledger(-self._bal_amount, description)
+            self.add_ledger(-amt, description)
             return True
         return False
 
     def get_balance(self) -> float:
         return self._bal_amount
 
-    def transfer(self, amt, bud_cat) -> bool:
+    def transfer(self, amt: Union[float, int], bud_cat: "Category") -> bool:
         res = self.check_funds(amt)
         if res:
             self.withdraw(amt, f"Transfer to {bud_cat}")
@@ -44,10 +52,8 @@ class Category:
             return True
         return False
 
-    def check_funds(self, amt) -> bool:
-        if amt > self._bal_amount:
-            return False
-        return True
+    def check_funds(self, amt: Union[float, int]) -> bool:
+        return amt <= self._bal_amount
 
 
 def create_spend_chart(categories):
