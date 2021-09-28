6c9365b1f7c79f7959d6df4bb3a0c1caa554acb5
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-09-28 18:45:41 +0530

    Methods are added to the category class

--- budget.py
+++ budget.py
@@ -1,7 +1,42 @@
+"""
+*************Food*************
+initial deposit        1000.00
+groceries               -10.15
+restaurant and more foo -15.89
+Transfer to Clothing    -50.00
+Total: 923.96
+"""
+
+
 class Category:
+    def __init__(self, category) -> None:
+        self.category = category
+        self.ledger = []
+        self._bal_amount = 0.00
 
+    def deposit(self, amt, description=None) -> None:
+        self._bal_amount += amt
 
+    def withdraw(self, amt, description=None) -> bool:
+        res = self.check_funds(amt)
+        if res:
+            self._bal_amount -= amt
+            return True
+        return False
 
+    def get_balance(self) -> float:
+        return self._bal_amount
+
+    def transfer(self, amt, bud_cat) -> bool:
+        res = self.check_funds(amt)
+        if res:
+            return True
+        return False
+
+    def check_funds(self, amt) -> bool:
+        if amt > self._bal_amount:
+            return False
+        return True
 
 
 def create_spend_chart(categories):
